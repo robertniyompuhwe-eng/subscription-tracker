b152b3ba2e66b204523319b1306b3b2c220f6cf9
Robert <robertniyompuhwe@gmail.com>
2026-01-22 08:38:42 +0200

    the workflow added to my project

--- controller/workflow.controller.js
+++ controller/workflow.controller.js
@@ -1,18 +1,30 @@
-import {createRequire} from 'module'
-import { useContext } from 'react'
-import { isContext } from 'vm'
-import Subscription from '../models/subscription.model'
-const require=createRequire(import.meta.url)
-const {serve}=require('@upstash/workflow/express')
-export const sendReminder=serve(async(useContext)=>{
-    const {subscriptionId}=isContext.requestPayload;
-    const subscription=await fetchSubscription(useContext,subscriptionId)
-    if(!subscription|| subscription.status!=active) return
-    const renewalDate=new Date(subscription.renewalDate)
-})
+import { createRequire } from "module";
+import Subscription from "../models/subscription.model.js";
 
-const fetchSubscription=async (isContext,subscriptionId)=>{
-    return await isContext.run('get subscription0',()=>{
-        return Subscription.findById(subscriptionId).populate('User','name email')
-    })
-}
+const require = createRequire(import.meta.url);
+const { serve } = require("@upstash/workflow/express");
+
+export const sendReminder = serve(async (context) => {
+  const { subscriptionId } = context.requestPayload;
+
+  const subscription = await fetchSubscription(context, subscriptionId);
+
+  if (!subscription || subscription.status !== "active") return;
+
+  const renewalDate = new Date(subscription.renewalDate);
+
+});
+
+const fetchSubscription = async (context, subscriptionId) => {
+  return context.run("get subscription", async () => {
+    const subscription = await Subscription
+      .findById(subscriptionId)
+      .populate("user", "name email");
+
+    if (!subscription) return null;
+
+    return subscription;
+  });
+};
+
+
